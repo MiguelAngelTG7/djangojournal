9d43324834d04c95266fd5f795e86d40948c8d12
MiguelAngelTG7 <michael_angello@hotmail.com>
2024-11-19 10:32:20 -0500

    todo bien hasta aqui

--- journal/urls.py
+++ journal/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('journalapp/', include('journalapp.urls') )
+    path('journalapp/', include('journalapp.urls'))
 ]
